d6a46c8adcf2b100e8fb3b7e26bb2c562c1276e3
Ubuntu <ubuntu@ip-172-31-22-110.eu-west-1.compute.internal>
2025-02-04 11:11:40 +0000

    aktualziacja s3_bash.sh

--- s3_bash.sh
+++ s3_bash.sh
@@ -3,8 +3,14 @@
 # Pobranie Instance ID
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-# wyswietyla zapisane instance na konsoli
+# Pobranie Public IP i Private IP
+PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Wyswietla zapisane instance na konsoli
 echo "Pobrano Instance ID: $INSTANCE_ID"
+echo "Pobrano Public IP: $PUBLIC_IP"
+echo "Pobraon Private IP: $PRIVATE_IP"
 
 # Zapisanie do pliku
 echo "Instance ID: $INSTANCE_ID" > instance_info.txt
