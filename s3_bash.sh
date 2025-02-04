0c7f008faddceec9528e5b4787853ef3cad16059
Ubuntu <ubuntu@ip-172-31-22-110.eu-west-1.compute.internal>
2025-02-04 11:41:20 +0000

    aktualizacja s3_bash.sh

--- s3_bash.sh
+++ s3_bash.sh
@@ -7,12 +7,25 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
+# Pobieranie informacji z systemu 
+OS_INFO=$(cat /etc/os-release)
+
+# Pobieranie uzytkownikow
+
+USERS=$(getent passwd | grep -E 'bash|sh' | cut -d: -f1)
+
 # Wyswietla zapisane instance na konsoli
 echo "Pobrano Instance ID: $INSTANCE_ID"
 echo "Pobrano Public IP: $PUBLIC_IP"
 echo "Pobrano Private IP: $PRIVATE_IP"
+echo "Pobrano Operating System: $OS_INFO"
+echo "Pobrano Users with shell access: $USERS"
 
 # Zapisanie do pliku
 echo "Instance ID: $INSTANCE_ID" > instance_info.txt
-
+echo "Public IP: $PUBLIC_IP" >> instance_info.txt
+echo "Private IP: $PRIVATE_IP" >> instance_info.txt
+echo "Operating System: $OS_INFO" >> instance_info.txt
+echo "Users with shell access:" >> instance_info.txt
+echo "$USERS" >> instance_info.txt
 
